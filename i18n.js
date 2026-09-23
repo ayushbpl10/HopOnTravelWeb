ed4e91010dbe44f49275392af79f7ef28c5c149f
ayush_gupta7 <ayush.gupta7@dell.com>
2026-09-23 17:25:12 +0530

    feat: add new translation strings to i18n configuration

--- i18n.js
+++ i18n.js
@@ -2181,6 +2181,9 @@ async function detectLocationAndSetLanguage() {
         isRegional = true;
         regionName = 'Karnataka';
       }
+    } catch (_) {}
+  }
+
   // If user made a manual choice while geo-detection was in-flight, do not overwrite
   try {
     if (localStorage.getItem('site_lang_manual')) {
